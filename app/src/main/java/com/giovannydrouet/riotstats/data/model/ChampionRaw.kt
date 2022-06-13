60e750bd85b188e5f02b172e6f11f43910987967
GiovannyDrouet <giovannydrouet@gmail.com>
2022-06-13 18:26:54 +0200

    Add Kotlin Serialization

--- app/src/main/java/com/giovannydrouet/riotstats/data/model/ChampionRaw.kt
+++ app/src/main/java/com/giovannydrouet/riotstats/data/model/ChampionRaw.kt
@@ -1,10 +1,14 @@
 package com.giovannydrouet.riotstats.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ChampionRaw(
     val name: String,
     val image: ChampionImageRaw
 )
 
+@Serializable
 data class ChampionImageRaw(
     val full: String
 )
--- app/src/main/java/com/giovannydrouet/riotstats/data/model/ChampionResponse.kt
+++ app/src/main/java/com/giovannydrouet/riotstats/data/model/ChampionResponse.kt
@@ -1,5 +1,8 @@
 package com.giovannydrouet.riotstats.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ChampionResponse(
     val data: Map<String, ChampionRaw>
 )
