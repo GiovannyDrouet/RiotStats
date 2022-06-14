ece155c551aaa813385bfa2466ad048ad324a194
GiovannyDrouet <giovannydrouet@gmail.com>
2022-06-14 15:51:08 +0200

    Just some comments to study and possible changes

--- app/src/main/java/com/giovannydrouet/riotstats/domain/GetChampionsUseCase.kt
+++ app/src/main/java/com/giovannydrouet/riotstats/domain/GetChampionsUseCase.kt
@@ -6,6 +6,9 @@ import javax.inject.Inject
 
 //Class with the function that receives the list sent by data/hilt and stores it in domain to
 //share it with presentation
+
+//You use an Interface instead of using directly 'ChampionRepositoryImpl' to avoid a dependency
+//of data
 class GetChampionsUseCase @Inject constructor(private val championRepository: ChampionRepository) {
 
     suspend fun getChampions(): List<Champion> {
